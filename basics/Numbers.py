a398835792a5f4c387ac2d3ce8ec2d7a207bd1f0
oviyashreelati <oviyashree.lati@gmail.com>
2025-12-31 11:14:41 +0530

    second commit

---
+++ basics/Numbers.py
@@ -0,0 +1,19 @@
+age = "10"
+y = "Hello, world!"
+name = "Oviya"
+
+# print ("My Name is Sandeep.")
+
+def add_numbers(a, b):
+	# """Return the sum of two numbers a and b."""
+	return a + b
+
+
+if __name__ == "__main__":
+	print("My name is " + name + " and I am " + age + " years old.")
+	# example usages
+	a=5
+	b=5
+	x=add_numbers(a, b)
+	print("Adding Numbers result : ->", x)
+	
--- basics/hello.py
+++ basics/hello.py
@@ -1,1 +1,9 @@
-print("Jhswitha is a very good girl.")
+age = "10"
+y = "Hello, world!"
+name = "Oviya"
+
+# print ("My Name is Sandeep.")
+
+
+print("My name is " + name + " and I am " + age + " years old.")
+
